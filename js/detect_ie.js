ac0b141f7335f99244018d8c9808c1a1bff41220
Alve Svarén <alve@hotmail.se>
2023-05-26 18:04:02 +0200

    Update for 2023

--- js/detect_ie.js
+++ js/detect_ie.js
@@ -1,6 +1,11 @@
 var isIe = window.navigator.userAgent.indexOf("Trident") > -1;
 
-if (isIe && (sessionStorage === undefined || !sessionStorage.getItem("hasSeenIEPopup"))) {
-    alert("Flera funktioner på denna sidan fungerar inte med Internet Explorer, överväg att använda exempelvis Mozilla Firefox eller Google Chrome istället.");
+if (
+    isIe &&
+    (sessionStorage === undefined || !sessionStorage.getItem("hasSeenIEPopup"))
+) {
+    alert(
+        "Flera funktioner på denna sidan fungerar inte med Internet Explorer, överväg att använda exempelvis Mozilla Firefox eller Google Chrome istället."
+    );
     sessionStorage.setItem("hasSeenIEPopup", true);
 }
--- js/switch.js
+++ js/switch.js
@@ -3,7 +3,9 @@ const states = document.querySelectorAll(".switch a");
 
 /** @param {string} hash */
 function setSwitch(hash) {
-    document.querySelectorAll(".switch-section").forEach((element) => element.classList.add("hidden"));
+    document
+        .querySelectorAll(".switch-section")
+        .forEach((element) => element.classList.add("hidden"));
     document.querySelector(hash).classList.remove("hidden");
     states.forEach((state) => {
         state.classList.remove("selected");
